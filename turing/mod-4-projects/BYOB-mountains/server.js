098b59b4635f9f1cb2f1439742753327410a69e0
Tim <tbrandle53@gmail.com>
2017-05-18 15:47:26 -0600

    JWT configured and working.  All patch, post, and delete routes protected

--- turing/mod-4-projects/BYOB-mountains/server.js
+++ turing/mod-4-projects/BYOB-mountains/server.js
@@ -1,26 +1,61 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken')
+const config = require('dotenv').config().parsed;
 
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set('secretKey', config.CLIENT_SECRET);
 app.set('port', process.env.PORT || 3000);
+
+if (!config.CLIENT_SECRET || !config.USERNAME || !config.PASSWORD) {
+  throw 'Make sure you have a CLIENT_SECRET, USERNAME, and PASSWORD in your .env file'
+}
 
 app.listen(app.get('port'), () => {
   console.log(`port is running on ${app.get('port')}.`);
 });
 
-/********** GET ************/
+const checkAuth = (request, response, next) => {
+  const token = request.body.token ||
+                request.param('token') ||
+                request.headers['authorization'];
 
-app.get('/', (req, res) => {
-  console.log("oh hey");
-})
+  if (token) {
+    jwt.verify(token, app.get('secretKey'), (error, decoded) => {
+        // If the token is invalid or expired, respond with an error
+        if (error) {
+          return response.status(403).send({
+            success: false,
+            message: 'Invalid authorization token.'
+          });
+        }
+
+        // If the token is valid, save the decoded version to the
+        // request for use in other routes & continue on with next()
+        else {
+          request.decoded = decoded;
+          next();
+        }
+      });
+  }
+
+  else {
+    return response.status(403).send({
+      success: false,
+      message: 'You must be authorized to hit this endpoint'
+    });
+  }
+};
+
+/********** GET ************/
 
 app.get('/api/v1/mountains', (req, res) => {
   const heightQuery = req.query.height_ft
@@ -54,22 +89,39 @@ app.get('/api/v1/:id/mountain_range', (req, res) => {
     .catch(error => console.log(error))
 })
 
-
-
 /********** POST ************/
 
+app.post('/authenticate', (request, response) => {
+  const user = request.body;
 
-app.post('/api/v1/mountains', (req, res) => {
+  if (user.username !== config.USERNAME || user.password !== config.PASSWORD) {
+    response.status(403).send({
+      success: false,
+      message: 'Invalid Credentials'
+    });
+  }
+  // If the credentials are accurate, create a token and send it back
+  else {
+    let token = jwt.sign(user, app.get('secretKey'), {
+      expiresIn: 172800 // expires in 48 hours
+    });
 
-  // need better error handling
+    response.json({
+      success: true,
+      username: user.username,
+      token: token
+    });
+  }
+});
 
-  const mountain = req.body
-    database('mountains').insert(mountain, 'id')
-      .then(mountain => res.status(201).json({ id: mountain[0] }))
-      .catch(error=> console.log(error))
+app.post('/api/v1/mountains', checkAuth, (req, res) => {
+  const { mountain } = req.body
+  database('mountains').insert(mountain, 'id')
+    .then(mountain => res.status(201).json({ id: mountain[0] }))
+    .catch(error=> console.log(error))
 })
 
-app.post('/api/v1/ranges', (req, res) => {
+app.post('/api/v1/ranges', checkAuth, (req, res) => {
   const range = req.body
   database('range').insert(range, 'id')
     .then(range => res.status(201).json({ id: range[0] }))
@@ -78,26 +130,23 @@ app.post('/api/v1/ranges', (req, res) => {
 
 /********** PATCH ************/
 
-app.patch('/api/v1/mountains', (req, res) => {
+app.patch('/api/v1/mountains', checkAuth, (req, res) => {
   const { mountain, id } = req.body
   database('mountains').where('id', id).update({ mountain })
     .then(mountain => res.status(202).json(mountain))
     .catch(error => console.log(error))
 })
 
-app.patch('/api/v1/ranges', (req, res) => {
+app.patch('/api/v1/ranges', checkAuth, (req, res) => {
   const { range, id } = req.body
   database('range').where('id', id).update({ range })
     .then(range => res.status(202).json(range))
     .catch(error => console.log(error))
 })
 
-
-
-
 /********** DELETE ************/
 
-app.delete('/api/v1/mountains', (req, res) => {
+app.delete('/api/v1/mountains', checkAuth, (req, res) => {
   database('mountains').where('id', req.body.id).del()
     .catch(error => console.log(error))
 })
